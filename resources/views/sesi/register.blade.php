32c995a4aa8c1fa80b820bc8044695b0de0e4cbb
unknown <zaynfirman2@gmail.com>
2023-09-05 14:07:51 +0700

    login logout register

---
+++ resources/views/sesi/register.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.aplikasi')
+
+@section('konten')
+    <div class="w-50 center border rounded px-3 py-3 mx-auto">
+        <h1>Register</h1>
+        <form action="/sesi/create" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Nama</label>
+            <input type="text" name="name" class="form-control" value="{{ Session::get('name') }}">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" name="email" class="form-control" value="{{ Session::get('email') }}">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control">
+        </div>
+        <div class="mb-3 d-grid">
+            <button name="submit" type="submit" class="btn btn-primary">Register</button>
+        </div>
+    </form>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\HalamanController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,9 @@ Route::resource('siswa', SiswaController::class);
 Route::get('/', [HalamanController::class, 'index']);
 Route::get('/tentang', [HalamanController::class, 'tentang']);
 Route::get('/kontak', [HalamanController::class, 'kontak']);
+
+Route::get('/sesi',[SessionController::class, 'index']);
+Route::post('/sesi/login',[SessionController::class, 'login']);
+Route::get('/sesi/logout', [SessionController::class, 'logout']);
+Route::get('/sesi/register',[SessionController::class, 'register']);
+Route::post('/sesi/create', [SessionController::class, 'create']);
